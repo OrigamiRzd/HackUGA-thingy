e87ad374c5c4eaadc6e9dba70675a29c833c64ff
TheJadeRabbit <gc11ie9mff4@gmail.com>
2015-10-24 12:47:19 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -2,14 +2,6 @@ public class Driver{
 	
 	public static void main(String args[]){
 		
-		ClarifaiClient clarifai = new ClarifaiClient(APP_ID, APP_SECRET);
-		List<RecognitionResult> results =
-    		clarifai.recognize(new RecognitionRequest(new File("kittens.jpg")));
-
-		for (Tag tag : results.get(0).getTags()) {
-  		System.out.println(tag.getName() + ": " + tag.getProbability());
-}
-		
 	}
 	
 }
